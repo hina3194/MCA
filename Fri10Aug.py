065a89b6598f3c3abb3de9d64b28e420925a5330
hina3194 <40022161+hina3194@users.noreply.github.com>
2018-08-20 21:26:54 +0530

    Add files via upload

---
+++ Fri10Aug.py
@@ -0,0 +1,53 @@
+p='spam eggs'
+print(p)
+q='doesn\'t'
+print(q)
+r="doesn't"
+print(r)
+s='"Yes,"he said'
+print(s)
+t='"Isn\'t,"she said'
+print(t)
+v=('I\'m in kolkata but I don\'t kmow that"You are also here".I\'ll see you in howrah!. But I\'ll request')
+print(v)
+print(isinstance(4, object))
+print(isinstance("hello", object))
+isinstance(None, object)
+print(isinstance([1,2,3],object))
+print((4).__sizeof__())
+def compute(a,b,c):
+    return (a + b) * c
+b=compute(5,2,3)
+c=compute([1],[2,3],2)
+print(b)
+print(c)
+print(compute('l', 'olo',4))
+print(1 == 1.0)
+print(type(1) != type(1.0))
+int != float
+g = "Hello world!"
+print(g)
+print(g[4])
+print('world' in g)
+print(len(g))
+print(g.find('lo'))
+print(g.replace('llo','y'))
+print(g.startswith('hell'))
+print(g.isalpha())
+print(g.lower())
+print(g.title())
+print(g.upper())
+print(g.strip())
+print(g.strip('H'))
+print(g.split())
+print('10/08/2018'.split(sep='/'))
+print(g.join(['john',',','michael']))
+print('{}{}'.format('monty',  'python'))
+print("{0} can be {1} {0}s".format("strings", "formatted"))
+print("{name} loves {food}".format(name="sam", food="python"))
+print("{} squares is{}". format(5, 5 ** 2))
+                        
+
+
+
+
